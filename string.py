fe875da979065df13587221d7f28408c82d45b3c
Mahendra <178223873+mahendra-kumar-ravi@users.noreply.github.com>
2025-02-06 21:56:56 +0530

    string file is added

---
+++ string.py
@@ -0,0 +1,18 @@
+# hi ="how are you "
+# name = 'mahi'
+# greet = hi+name
+# # print(greet)
+
+# greeting = hi +(" "+name )
+# print(greeting)
+
+# silly = greeting = hi +(" "+name )*3
+# print(silly)
+
+#type:ignore
+x =1 
+print(x)
+x_str = str(x)
+print("my fav number is",x,".","x=",x)
+print("my fav number is",x_str+"."+"x="+x_str)
+print("my fav number is"+x_str+"."+ "x="+x_str)
